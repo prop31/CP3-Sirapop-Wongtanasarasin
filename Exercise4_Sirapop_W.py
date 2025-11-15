358ea5f971e9b1a4a5905737a75b8191e7cfcd57
prop31 <116890308+prop31@users.noreply.github.com>
2025-11-15 09:52:14 +0700

    Add files via upload

---
+++ Exercise4_Sirapop_W.py
@@ -0,0 +1,13 @@
+
+FE = (float(input('Enter the Foundation English score :')))
+GB = (float(input('Enter the General Business score :')))
+IT = (float(input('Enter the Introduction to Computer Systems score :')))
+CP = (float(input('Enter the Computer Programming score :')))
+
+print("--- Your Score ---")
+print('Foundation English :',FE)
+print('General Business :',GB)
+print('Introduction to Computer Systems :',IT)
+print('Computer Programming :',CP)
+
+
