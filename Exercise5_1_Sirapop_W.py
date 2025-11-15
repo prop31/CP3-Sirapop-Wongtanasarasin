5fad11399b58a692d4a8a02be265c216a001fcd8
prop31 <116890308+prop31@users.noreply.github.com>
2025-11-15 17:07:11 +0700

    Add files via upload

---
+++ Exercise5_1_Sirapop_W.py
@@ -0,0 +1,7 @@
+x = int(input("เลขที่ 1 :"))
+y = int(input("เลขที่ 2 :"))
+
+print((x),"+",(y) ,"=" , x + y)
+print((x),"-",(y) ,"=" , x - y)
+print((x),"*",(y) ,"=" , x * y)
+print((x),"/",(y) ,"=" , x / y)
---
+++ Exercise5_2_Sirapop_W.py
@@ -0,0 +1,6 @@
+s = int(input("ระยะทาง km :"))
+t = int(input("เวลา hr :"))
+s > 1
+t > 1
+
+print("ความเร็ว =" ,s/t ,"km/hr")
